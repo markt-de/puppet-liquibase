4e02717acab8dfa7ae565c0e32ea04689a5ee4ad
baptiste.jammet <baptiste.jammet@ville-noumea.nc>
2016-01-13 12:51:24 +1100

    #15531 : Modif valeur pas défaut staging_path

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 # See README.md
 class liquibase(
-  $staging_path       = /opt/staging/,
+  $staging_path       = /opt/puppet/staging/,
   #version de liquibase
   $version 	          =  $liquibase::version,
   # Versions des drivers jdbc
