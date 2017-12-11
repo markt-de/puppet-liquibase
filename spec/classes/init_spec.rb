10fe8a9c9b44e5b575bc21513dae5b60cb40f99f
Gault Mélanie <melanie.gault@ville-noumea.nc>
2017-12-11 13:02:22 +1100

    #42923: liquibase rubocop errors

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 describe 'liquibase' do
 
   context 'with defaults for all parameters' do
-    it { should contain_class('liquibase') }
+    it { is_expected.to contain_class('liquibase') }
   end
 end
