a366479dafef5b1b4e2c10577b1e428fe2862716
Gault Mélanie <melanie.gault@ville-noumea.nc>
2017-12-11 13:30:14 +1100

    #42923: liquibase variable for staging

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -13,10 +13,11 @@ describe 'liquibase' do
       is_virtual: 'true',
       fqdn: 'svq-nonexistant.domain',
       architecture: 'x64',
-      selinux: true
+      selinux: true,
+      staging_http_get: 'http://yum'
     }
   end
-  
+
   context 'with defaults for all parameters' do
     it { is_expected.to contain_class('liquibase') }
   end
