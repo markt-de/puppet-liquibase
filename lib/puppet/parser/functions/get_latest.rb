df4dc367bb45a4052f053ed90c519661efdf7098
Gault Mélanie <melanie.gault@ville-noumea.nc>
2017-12-11 13:06:13 +1100

    #42923: liquibase rubocop errors

--- lib/puppet/parser/functions/get_latest.rb
+++ lib/puppet/parser/functions/get_latest.rb
@@ -1,15 +1,14 @@
 require 'net/http'
 require 'net/https' if RUBY_VERSION < '1.9'
 require 'uri'
-
 module Puppet::Parser::Functions
   newfunction(:get_latest, type: rvalue) do |args|
-  u = URI.parse(args[0])
-  h = Net::HTTP.new u.host, u.port
-  h.use_ssl = u.scheme == 'https'
-  head = h.start do |ua|
-    ua.head u.path
-  end
-  head['location'].sub('https://github.com/liquibase/liquibase/releases/tag/liquibase-parent-', '')
+    u = URI.parse(args[0])
+    h = Net::HTTP.new u.host, u.port
+    h.use_ssl = u.scheme == 'https'
+    head = h.start do |ua|
+      ua.head u.path
+    end
+    head['location'].sub('https://github.com/liquibase/liquibase/releases/tag/liquibase-parent-', '')
   end
 end
