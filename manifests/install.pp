047aca1999a35d8a701681b9c7c9cb10aacddef3
Gault Mélanie <melanie.gault@ville-noumea.nc>
2017-12-11 12:50:39 +1100

    #42923: liquibase lint top-scope variable

--- manifests/install.pp
+++ manifests/install.pp
@@ -43,9 +43,9 @@ class liquibase::install inherits liquibase {
   # See README.md
   include staging
 
-  staging::file { "liquibase-${version}-bin.tar.gz":
+  staging::file { "liquibase-${liquibase::version}-bin.tar.gz":
     environment => $environment,
-    source      => "https://github.com/liquibase/liquibase/releases/download/liquibase-parent-${version}/liquibase-${version}-bin.tar.gz",
+    source      => "https://github.com/liquibase/liquibase/releases/download/liquibase-parent-${liquibase::version}/liquibase-${liquibase::version}-bin.tar.gz",
   }
 
   file { '/opt/apps/liquibase/':
@@ -53,23 +53,23 @@ class liquibase::install inherits liquibase {
     recurse => true,
     purge   => true,
     force   => true,
-    require => Staging::File["liquibase-${version}-bin.tar.gz"],
+    require => Staging::File["liquibase-${liquibase::version}-bin.tar.gz"],
   }
 
-  file { "/opt/apps/liquibase/liquibase-${version}":
+  file { "/opt/apps/liquibase/liquibase-${liquibase::version}":
     ensure  => directory,
-    require => Staging::File["liquibase-${version}-bin.tar.gz"],
+    require => Staging::File["liquibase-${liquibase::version}-bin.tar.gz"],
   }
 
-  staging::extract { "liquibase-${version}-bin.tar.gz":
-    target  => "/opt/apps/liquibase/liquibase-${version}",
-    require => Staging::File["liquibase-${version}-bin.tar.gz"],
+  staging::extract { "liquibase-${liquibase::version}-bin.tar.gz":
+    target  => "/opt/apps/liquibase/liquibase-${liquibase::version}",
+    require => Staging::File["liquibase-${liquibase::version}-bin.tar.gz"],
   }
 
   # Creation des liens synboliques
   file { '/opt/liquibase':
     ensure => 'link',
-    target => "/opt/apps/liquibase/liquibase-${version}",
+    target => "/opt/apps/liquibase/liquibase-${liquibase::version}",
   }
 
   file { '/usr/bin/liquibase':
@@ -83,130 +83,130 @@ class liquibase::install inherits liquibase {
     owner   => root,
     group   => root,
     source  => 'puppet:///modules/liquibase/liquibase_env.sh',
-    require => Staging::Extract["liquibase-${version}-bin.tar.gz"],
+    require => Staging::Extract["liquibase-${liquibase::version}-bin.tar.gz"],
   }
 
   # Copie des drivers
   # PostgreSQL
-  staging::file { "postgresql-${postgresql_version}.jar":
-    environment => $environment,
-    source      => "http://central.maven.org/maven2/org/postgresql/postgresql/${postgresql_version}/postgresql-${postgresql_version}.jar",
-    require     => Staging::Extract["liquibase-${version}-bin.tar.gz"],
+  staging::file { "postgresql-${liquibase::postgresql_version}.jar":
+    environment => $liquibase::environment,
+    source      => "http://central.maven.org/maven2/org/postgresql/postgresql/${liquibase::postgresql_version}/postgresql-${liquibase::postgresql_version}.jar",
+    require     => Staging::Extract["liquibase-${liquibase::version}-bin.tar.gz"],
   }
 
-  exec { "copy_postgresql-${postgresql_version}.jar":
-    command => "/bin/cp ${staging::path}/liquibase/postgresql-${postgresql_version}.jar /opt/apps/liquibase/liquibase-${version}/lib/",
+  exec { "copy_postgresql-${liquibase::postgresql_version}.jar":
+    command => "/bin/cp ${staging::path}/liquibase/postgresql-${liquibase::postgresql_version}.jar /opt/apps/liquibase/liquibase-${liquibase::version}/lib/",
     path    => '/usr/local/bin/:/bin/',
-    creates => "/opt/liquibase-${version}/lib/postgresql-${postgresql_version}.jar",
-    require => [Staging::Extract["liquibase-${version}-bin.tar.gz"], Staging::File["postgresql-${postgresql_version}.jar"]]
+    creates => "/opt/liquibase-${liquibase::version}/lib/postgresql-${liquibase::postgresql_version}.jar",
+    require => [Staging::Extract["liquibase-${liquibase::version}-bin.tar.gz"], Staging::File["postgresql-${liquibase::postgresql_version}.jar"]]
   }
 
   # Derby
-  staging::file { "derbyclient-${derby_version}.jar":
-    environment => $environment,
-    source      => "http://central.maven.org/maven2/org/apache/derby/derbyclient/${derby_version}/derbyclient-${derby_version}.jar",
-    require     => Staging::Extract["liquibase-${version}-bin.tar.gz"],
+  staging::file { "derbyclient-${liquibase::derby_version}.jar":
+    environment => $liquibase::environment,
+    source      => "http://central.maven.org/maven2/org/apache/derby/derbyclient/${liquibase::derby_version}/derbyclient-${liquibase::derby_version}.jar",
+    require     => Staging::Extract["liquibase-${liquibase::version}-bin.tar.gz"],
   }
 
-  exec { "copy_derbyclient-${derby_version}.jar":
-    command => "/bin/cp ${staging::path}/liquibase/derbyclient-${derby_version}.jar /opt/apps/liquibase/liquibase-${version}/lib/",
+  exec { "copy_derbyclient-${liquibase::derby_version}.jar":
+    command => "/bin/cp ${staging::path}/liquibase/derbyclient-${liquibase::derby_version}.jar /opt/apps/liquibase/liquibase-${liquibase::version}/lib/",
     path    => '/usr/local/bin/:/bin/',
-    creates => "/opt/apps/liquibase/liquibase-${version}/lib/derbyclient-${derby_version}.jar",
-    require => [Staging::Extract["liquibase-${version}-bin.tar.gz"], Staging::File["derbyclient-${derby_version}.jar"]],
+    creates => "/opt/apps/liquibase/liquibase-${liquibase::version}/lib/derbyclient-${liquibase::derby_version}.jar",
+    require => [Staging::Extract["liquibase-${liquibase::version}-bin.tar.gz"], Staging::File["derbyclient-${liquibase::derby_version}.jar"]],
   }
 
   # H2
-  staging::file { "h2-${h2_version}.jar":
-    environment => $environment,
-    source      => "http://central.maven.org/maven2/com/h2database/h2/${h2_version}/h2-${h2_version}.jar",
-    require     => Staging::Extract["liquibase-${version}-bin.tar.gz"],
+  staging::file { "h2-${liquibase::h2_version}.jar":
+    environment => $liquibase::environment,
+    source      => "http://central.maven.org/maven2/com/h2database/h2/${liquibase::h2_version}/h2-${liquibase::h2_version}.jar",
+    require     => Staging::Extract["liquibase-${liquibase::version}-bin.tar.gz"],
   }
 
-  exec { "copy_h2-${h2_version}.jar":
-    command => "/bin/cp ${staging::path}/liquibase/h2-${h2_version}.jar /opt/apps/liquibase/liquibase-${version}/lib/",
+  exec { "copy_h2-${liquibase::h2_version}.jar":
+    command => "/bin/cp ${staging::path}/liquibase/h2-${liquibase::h2_version}.jar /opt/apps/liquibase/liquibase-${liquibase::version}/lib/",
     path    => '/usr/local/bin/:/bin/',
-    creates => "/opt/apps/liquibase/liquibase-${version}/lib/h2-${h2_version}.jar",
-    require => [Staging::Extract["liquibase-${version}-bin.tar.gz"], Staging::File["h2-${h2_version}.jar"]],
+    creates => "/opt/apps/liquibase/liquibase-${liquibase::version}/lib/h2-${liquibase::h2_version}.jar",
+    require => [Staging::Extract["liquibase-${liquibase::version}-bin.tar.gz"], Staging::File["h2-${liquibase::h2_version}.jar"]],
   }
 
   # Hsqldb
-  staging::file { "hsqldb-${hsqldb_version}.jar":
-    environment => $environment,
-    source      => "http://central.maven.org/maven2/org/hsqldb/hsqldb/${hsqldb_version}/hsqldb-${hsqldb_version}.jar",
-    require     => Staging::Extract["liquibase-${version}-bin.tar.gz"],
+  staging::file { "hsqldb-${liquibase::hsqldb_version}.jar":
+    environment => $liquibase::environment,
+    source      => "http://central.maven.org/maven2/org/hsqldb/hsqldb/${liquibase::hsqldb_version}/hsqldb-${liquibase::hsqldb_version}.jar",
+    require     => Staging::Extract["liquibase-${liquibase::version}-bin.tar.gz"],
   }
 
-  exec { "copy_hsqldb-${hsqldb_version}.jar":
-    command => "/bin/cp ${staging::path}/liquibase/hsqldb-${hsqldb_version}.jar /opt/apps/liquibase/liquibase-${version}/lib/",
+  exec { "copy_hsqldb-${liquibase::hsqldb_version}.jar":
+    command => "/bin/cp ${staging::path}/liquibase/hsqldb-${liquibase::hsqldb_version}.jar /opt/apps/liquibase/liquibase-${liquibase::version}/lib/",
     path    => '/usr/local/bin/:/bin/',
-    creates => "/opt/apps/liquibase/liquibase-${version}/lib/hsqldb-${hsqldb_version}.jar",
-    require => [Staging::Extract["liquibase-${version}-bin.tar.gz"], Staging::File["hsqldb-${hsqldb_version}.jar"]],
+    creates => "/opt/apps/liquibase/liquibase-${liquibase::version}/lib/hsqldb-${liquibase::hsqldb_version}.jar",
+    require => [Staging::Extract["liquibase-${liquibase::version}-bin.tar.gz"], Staging::File["hsqldb-${liquibase::hsqldb_version}.jar"]],
   }
 
   # Jtds
-  staging::file { "jtds-${jtds_version}.jar":
-    environment => $environment,
-    source      => "http://central.maven.org/maven2/net/sourceforge/jtds/jtds/${jtds_version}/jtds-${jtds_version}.jar",
-    require     => Staging::Extract["liquibase-${version}-bin.tar.gz"],
+  staging::file { "jtds-${liquibase::jtds_version}.jar":
+    environment => $liquibase::environment,
+    source      => "http://central.maven.org/maven2/net/sourceforge/jtds/jtds/${liquibase::jtds_version}/jtds-${liquibase::jtds_version}.jar",
+    require     => Staging::Extract["liquibase-${liquibase::version}-bin.tar.gz"],
   }
 
-  exec { "copy_jtds-${jtds_version}.jar":
-    command => "/bin/cp ${staging::path}/liquibase/jtds-${jtds_version}.jar /opt/apps/liquibase/liquibase-${version}/lib/",
+  exec { "copy_jtds-${liquibase::jtds_version}.jar":
+    command => "/bin/cp ${staging::path}/liquibase/jtds-${liquibase::jtds_version}.jar /opt/apps/liquibase/liquibase-${liquibase::version}/lib/",
     path    => '/usr/local/bin/:/bin/',
-    creates => "/opt/apps/liquibase/liquibase-${version}/lib/jtds-${jtds_version}.jar",
-    require => [Staging::Extract["liquibase-${version}-bin.tar.gz"], Staging::File["jtds-${jtds_version}.jar"]],
+    creates => "/opt/apps/liquibase/liquibase-${liquibase::version}/lib/jtds-${liquibase::jtds_version}.jar",
+    require => [Staging::Extract["liquibase-${liquibase::version}-bin.tar.gz"], Staging::File["jtds-${liquibase::jtds_version}.jar"]],
   }
 
   # MySql
-  staging::file { "mysql-connector-java-${mysql_version}.jar":
-    environment => $environment,
-    source      => "http://central.maven.org/maven2/mysql/mysql-connector-java/${mysql_version}/mysql-connector-java-${mysql_version}.jar",
-    require     => Staging::Extract["liquibase-${version}-bin.tar.gz"],
+  staging::file { "mysql-connector-java-${liquibase::mysql_version}.jar":
+    environment => $liquibase::environment,
+    source      => "http://central.maven.org/maven2/mysql/mysql-connector-java/${liquibase::mysql_version}/mysql-connector-java-${liquibase::mysql_version}.jar",
+    require     => Staging::Extract["liquibase-${liquibase::version}-bin.tar.gz"],
   }
 
-  exec { "copy_mysql-connector-java-${mysql_version}.jar":
-    command => "/bin/cp ${staging::path}/liquibase/mysql-connector-java-${mysql_version}.jar /opt/apps/liquibase/liquibase-${version}/lib/",
+  exec { "copy_mysql-connector-java-${liquibase::mysql_version}.jar":
+    command => "/bin/cp ${staging::path}/liquibase/mysql-connector-java-${liquibase::mysql_version}.jar /opt/apps/liquibase/liquibase-${liquibase::version}/lib/",
     path    => '/usr/local/bin/:/bin/',
-    creates => "/opt/apps/liquibase/liquibase-${version}/lib/mysql-connector-java-${mysql_version}.jar",
-    require => [Staging::Extract["liquibase-${version}-bin.tar.gz"], Staging::File["mysql-connector-java-${mysql_version}.jar"]],
+    creates => "/opt/apps/liquibase/liquibase-${liquibase::version}/lib/mysql-connector-java-${liquibase::mysql_version}.jar",
+    require => [Staging::Extract["liquibase-${liquibase::version}-bin.tar.gz"], Staging::File["mysql-connector-java-${liquibase::mysql_version}.jar"]],
   }
 
   # Jt400
-  staging::file { "jt400-${jt400_version}.jar":
-    environment => $environment,
-    source      => "http://central.maven.org/maven2/net/sf/jt400/jt400/${jt400_version}/jt400-${jt400_version}.jar",
-    require     => Staging::Extract["liquibase-${version}-bin.tar.gz"],
+  staging::file { "jt400-${liquibase::jt400_version}.jar":
+    environment => $liquibase::environment,
+    source      => "http://central.maven.org/maven2/net/sf/jt400/jt400/${liquibase::jt400_version}/jt400-${liquibase::jt400_version}.jar",
+    require     => Staging::Extract["liquibase-${liquibase::version}-bin.tar.gz"],
   }
 
-  exec { "copy_jt400-${jt400_version}.jar":
-    command => "/bin/cp ${staging::path}/liquibase/jt400-${jt400_version}.jar /opt/apps/liquibase/liquibase-${version}/lib/",
+  exec { "copy_jt400-${liquibase::jt400_version}.jar":
+    command => "/bin/cp ${staging::path}/liquibase/jt400-${liquibase::jt400_version}.jar /opt/apps/liquibase/liquibase-${liquibase::version}/lib/",
     path    => '/usr/local/bin/:/bin/',
-    creates => "/opt/apps/liquibase/liquibase-${version}/lib/jt400-${jt400_version}.jar",
-    require => [Staging::Extract["liquibase-${version}-bin.tar.gz"], Staging::File["jt400-${jt400_version}.jar"]],
+    creates => "/opt/apps/liquibase/liquibase-${liquibase::version}/lib/jt400-${liquibase::jt400_version}.jar",
+    require => [Staging::Extract["liquibase-${liquibase::version}-bin.tar.gz"], Staging::File["jt400-${liquibase::jt400_version}.jar"]],
   }
 
   # MariaDB
-  staging::file { "mariadb-${mariadb_version}.jar":
-    environment => $environment,
-    source      => "http://central.maven.org/maven2/org/mariadb/jdbc/mariadb-java-client/${mariadb_version}/mariadb-java-client-${mariadb_version}.jar",
-    require     => Staging::Extract["liquibase-${version}-bin.tar.gz"],
+  staging::file { "mariadb-${liquibase::mariadb_version}.jar":
+    environment => $liquibase::environment,
+    source      => "http://central.maven.org/maven2/org/mariadb/jdbc/mariadb-java-client/${liquibase::mariadb_version}/mariadb-java-client-${liquibase::mariadb_version}.jar",
+    require     => Staging::Extract["liquibase-${liquibase::version}-bin.tar.gz"],
   }
 
-  exec { "copy_mariadb-${mariadb_version}.jar":
-    command => "/bin/cp ${staging::path}/liquibase/mariadb-${mariadb_version}.jar /opt/apps/liquibase/liquibase-${version}/lib/",
+  exec { "copy_mariadb-${liquibase::mariadb_version}.jar":
+    command => "/bin/cp ${staging::path}/liquibase/mariadb-${liquibase::mariadb_version}.jar /opt/apps/liquibase/liquibase-${liquibase::version}/lib/",
     path    => '/usr/local/bin/:/bin/',
-    creates => "/opt/apps/liquibase/liquibase-${version}/lib/mariadb-${mariadb_version}.jar",
-    require => [Staging::Extract["liquibase-${version}-bin.tar.gz"], Staging::File["mariadb-${mariadb_version}.jar"]],
+    creates => "/opt/apps/liquibase/liquibase-${liquibase::version}/lib/mariadb-${liquibase::mariadb_version}.jar",
+    require => [Staging::Extract["liquibase-${liquibase::version}-bin.tar.gz"], Staging::File["mariadb-${liquibase::mariadb_version}.jar"]],
   }
 
   # Sqlite
   # Copie du jar car seule cette version (3.7.8) fonctionne
-  file { "/opt/apps/liquibase/liquibase-${version}/lib/sqlite-jdbc-${sqlite_version}.jar":
+  file { "/opt/apps/liquibase/liquibase-${liquibase::version}/lib/sqlite-jdbc-${liquibase::sqlite_version}.jar":
     ensure  => present,
     owner   => root,
     group   => root,
-    source  => "puppet:///modules/liquibase/sqlite-jdbc-${sqlite_version}.jar",
-    require => Staging::Extract["liquibase-${version}-bin.tar.gz"],
+    source  => "puppet:///modules/liquibase/sqlite-jdbc-${liquibase::sqlite_version}.jar",
+    require => Staging::Extract["liquibase-${liquibase::version}-bin.tar.gz"],
   }
 
 }
