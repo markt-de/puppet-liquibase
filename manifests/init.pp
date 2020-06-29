6c91dbdc50d20ef82afaadd74a7457ea6326b8e1
mco-system <michael.costa@mcos.nc>
2020-06-30 10:26:43 +1100

    fix: variable inheritance from class liquibase::params

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,16 +3,16 @@ class liquibase(
   #version de liquibase
   $version            = $liquibase::version,
   # Versions des drivers jdbc
-  $postgresql_version = $liquibase::postgresql_version,
-  $derby_version      = $liquibase::derby_version,
-  $h2_version         = $liquibase::h2_version,
-  $hsqldb_version     = $liquibase::hsqldb_version,
-  $jtds_version       = $liquibase::jtds_version,
-  $mysql_version      = $liquibase::mysql_version,
-  $sqlite_version     = $liquibase::sqlite_version,
-  $jt400_version      = $liquibase::jt400_version,
-  $mariadb_version    = $liquibase::mariadb_version,
-  $environment        = $liquibase::environment,
+  $postgresql_version = $liquibase::params::postgresql_version,
+  $derby_version      = $liquibase::params::derby_version,
+  $h2_version         = $liquibase::params::h2_version,
+  $hsqldb_version     = $liquibase::params::hsqldb_version,
+  $jtds_version       = $liquibase::params::jtds_version,
+  $mysql_version      = $liquibase::params::mysql_version,
+  $sqlite_version     = $liquibase::params::sqlite_version,
+  $jt400_version      = $liquibase::params::jt400_version,
+  $mariadb_version    = $liquibase::params::mariadb_version,
+  $environment        = $liquibase::params::environment,
   ) inherits liquibase::params {
   class {'liquibase::install': } -> Class['liquibase']
 }
